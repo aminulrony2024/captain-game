3463e99691e3c318427f821870c071aa089e353d
aminulrony2024 <aminulrony2024@gmail.com>
2024-09-26 12:02:06 +0600

    first commit

---
+++ app/src/main/java/com/aminulrony/captaingame/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.aminulrony.captaingame
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aminulrony.captaingame.ui.theme.CaptainGameTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CaptainGameTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Aminul(
+
+                    )
+                }
+            }
+        }
+    }
+    @Composable
+    fun Aminul() {
+        val treasureFound = remember { mutableStateOf(0) }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun AminulPreview()
+    {
+        Aminul()
+    }
+}
