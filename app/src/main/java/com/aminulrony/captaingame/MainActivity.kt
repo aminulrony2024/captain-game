c78e1a24aa9953e43a12519747f4ce1d514fe9df
aminulrony2024 <aminulrony2024@gmail.com>
2024-09-27 13:12:21 +0600

    several button working properly

--- app/src/main/java/com/aminulrony/captaingame/MainActivity.kt
+++ app/src/main/java/com/aminulrony/captaingame/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aminulrony.captaingame.ui.theme.CaptainGameTheme
 import kotlin.random.Random
 
@@ -37,16 +40,39 @@ class MainActivity : ComponentActivity() {
         val treasureFound = remember { mutableStateOf(0) }
         val direction = remember { mutableStateOf("North")}
         Column {
-
+                Spacer(modifier = Modifier.height(60.dp))
                 Text(text = "Tresures found : ${treasureFound.value}")
                 Text(text = "Current direction we are going : ${direction.value}")
+            Row {
                 Button(onClick = {
                     direction.value = "East"
-                    if(Random.nextBoolean())
+                    if (Random.nextBoolean())
                         treasureFound.value += 1
                 }) {
                     Text("Sail East")
                 }
+                Button(onClick = {
+                    direction.value = "West"
+                    if (Random.nextBoolean())
+                        treasureFound.value += 1
+                }) {
+                    Text("Sail West")
+                }
+                Button(onClick = {
+                    direction.value = "North"
+                    if (Random.nextBoolean())
+                        treasureFound.value += 1
+                }) {
+                    Text("Sail North")
+                }
+                Button(onClick = {
+                    direction.value = "South"
+                    if (Random.nextBoolean())
+                        treasureFound.value += 1
+                }) {
+                    Text("Sail South")
+                }
+            }
         }
     }
     @Preview(showBackground = true)
